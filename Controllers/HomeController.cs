7d2d1192ea38bc24af88d703df0141675beb056e
Thiago Fernando <thiago.procorio@loglabdigital.com.br>
2020-12-14 12:51:44 -0400

    Criado Aplicação

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MeuApp.Models;
+using System.Drawing;
+using QRCoder;
+using System.IO;
+
+namespace MeuApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("qrcode")]
+        public IActionResult GetQrCode()
+        {
+            var imageQr = QrCodeGenerator.GenerateByteArray("https://docs.microsoft.com/pt-br/dotnet/");
+            return File(imageQr,"image/jpeg");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
